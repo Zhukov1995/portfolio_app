f8210e2e1417114ac1c7b7ccedf3ea4f564e228f
Roman Zhukov <ro.zhuckov2013@yandex.ru>
2023-03-31 15:05:10 +0300

    реализация динамического поведения хедера

--- src/App/Header/header.js
+++ src/App/Header/header.js
@@ -1,12 +1,33 @@
 import Navigation from '../Navigation/navigation';
+import { useState } from 'react';
 import { MBurger } from '../UI/Burger/burger';
 import Logo from './logo.svg';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import './header.scss';
 import './header.media.scss';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const burger = useSelector(state => state.burger);
+    const [oldScrollPosition,setOldScrollPosition] = useState(0);
+    const [flagPosition,setFlagPosition] = useState(true);
+  
+    window.onscroll = () => {
+        const scrollTopPosition = document.documentElement.scrollTop;
+        if ((oldScrollPosition > scrollTopPosition) && scrollTopPosition > 200) {
+            setFlagPosition(true);
+        } else if(scrollTopPosition < 200) {
+            setFlagPosition(true);
+        } else if(burger === true) {
+            setFlagPosition(true);
+        } else {
+            setFlagPosition(false);
+        }
+        setOldScrollPosition(scrollTopPosition)
+    }
+
+    const classHeader = flagPosition ? "header" : "header header_hidden";
 
     const headerAnimation = {
         hidden: {
@@ -25,6 +46,7 @@ const Header = () => {
             initial="hidden"
             whileInView="visible"
             viewport={{once: true}}
+            className={classHeader}
         >
             <Link to="/">
                 <motion.img src={Logo} variants={headerAnimation} custom={2}/>
